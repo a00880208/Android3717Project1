0578bef90138773bbbae168e723c20165248ef27
A00841554 <surplus.et@gmail.com>
2014-09-10 15:49:56 -0700

    Added ImageGridVIewActivity, and ImageSwipeViewActivity

--- app/src/main/java/tobycatapps/c3717/cst/bcit/ca/android3717project1/ImageGridViewActivity.java
+++ app/src/main/java/tobycatapps/c3717/cst/bcit/ca/android3717project1/ImageGridViewActivity.java
@@ -1,31 +1,24 @@
 package tobycatapps.c3717.cst.bcit.ca.android3717project1;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-
-public class MyActivity extends Activity {
+public class ImageGridViewActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-        Intent i = new Intent();
-        Bundle b = new Bundle();
-        Message m = new Message();
-        m.ob
+        setContentView(R.layout.activity_image_grid_view);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.image_grid_view, menu);
         return true;
     }
 
