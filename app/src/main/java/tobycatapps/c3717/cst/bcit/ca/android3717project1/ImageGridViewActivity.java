545c70d3d94fd5cc01d5140a34fea9ea57046875
A00841554 <surplus.et@gmail.com>
2014-09-12 19:14:31 -0700

    - created a grid view layout for ImageGridViewActivity - added code to parse the starting intent of ImageGridViewActivity.java

--- app/src/main/java/tobycatapps/c3717/cst/bcit/ca/android3717project1/ImageGridViewActivity.java
+++ app/src/main/java/tobycatapps/c3717/cst/bcit/ca/android3717project1/ImageGridViewActivity.java
@@ -1,17 +1,32 @@
 package tobycatapps.c3717.cst.bcit.ca.android3717project1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
 
 
 public class ImageGridViewActivity extends Activity {
 
+    // references to GUI views
+    /** reference to grid view that contains all the images */
+    private static GridView mImageGridView;
+
+    // starting intent keys and values
+    public static final String KEY_IMAGE_URIS = "KEY_IMAGE_URIS";
+    private String[] mImageURIs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_grid_view);
+        setContentView(R.layout.activity_image_grid_view);  // set activity's screen layout
+        initializeGUIReferences();
+
+        // extract information from starting intent
+        Intent i = getIntent();
+        mImageURIs = i.getStringArrayExtra(KEY_IMAGE_URIS);
     }
 
 
@@ -32,5 +47,13 @@ public class ImageGridViewActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * initializes all GUI references of this activity; initializes value of the GUI reference with
+     * the corresponding view in the layout.
+     */
+    private void initializeGUIReferences() {
+        mImageGridView = (GridView) findViewById(R.id.grid_view_images);
     }
 }
