94a6a093004f6000917ddf4da695c10be544e053
A00841554 <surplus.et@gmail.com>
2014-11-14 01:06:01 -0800

    - removed the search by tags feature - fixed the SearchActivity

--- app/src/main/java/tobycatapps/c3717/cst/bcit/ca/android3717project1/activity/SearchActivity.java
+++ app/src/main/java/tobycatapps/c3717/cst/bcit/ca/android3717project1/activity/SearchActivity.java
@@ -40,35 +40,35 @@ public class SearchActivity extends Activity {
     /** used to specify a uploader name filter for the search */
     private EditText uploaderNameFilterInput;
 
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput1;
-
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput3;
-
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput5;
-
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput7;
-
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput9;
-
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput2;
-
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput4;
-
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput6;
-
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput8;
-
-    /** used to activate or deactivate an image tag */
-    private CheckBox tagInput10;
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput1;
+//
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput3;
+//
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput5;
+//
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput7;
+//
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput9;
+//
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput2;
+//
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput4;
+//
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput6;
+//
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput8;
+//
+//    /** used to activate or deactivate an image tag */
+//    private CheckBox tagInput10;
 
 
 
@@ -123,16 +123,16 @@ public class SearchActivity extends Activity {
 
         // get tags user inputs
         ArrayList<String> tags = new ArrayList<String>();
-        if (tagInput1.isChecked()) tags.add(tagInput1.getText().toString());
-        if (tagInput3.isChecked()) tags.add(tagInput3.getText().toString());
-        if (tagInput5.isChecked()) tags.add(tagInput5.getText().toString());
-        if (tagInput7.isChecked()) tags.add(tagInput7.getText().toString());
-        if (tagInput9.isChecked()) tags.add(tagInput9.getText().toString());
-        if (tagInput2.isChecked()) tags.add(tagInput2.getText().toString());
-        if (tagInput4.isChecked()) tags.add(tagInput4.getText().toString());
-        if (tagInput6.isChecked()) tags.add(tagInput6.getText().toString());
-        if (tagInput8.isChecked()) tags.add(tagInput8.getText().toString());
-        if (tagInput10.isChecked()) tags.add(tagInput10.getText().toString());
+//        if (tagInput1.isChecked()) tags.add(tagInput1.getText().toString());
+//        if (tagInput3.isChecked()) tags.add(tagInput3.getText().toString());
+//        if (tagInput5.isChecked()) tags.add(tagInput5.getText().toString());
+//        if (tagInput7.isChecked()) tags.add(tagInput7.getText().toString());
+//        if (tagInput9.isChecked()) tags.add(tagInput9.getText().toString());
+//        if (tagInput2.isChecked()) tags.add(tagInput2.getText().toString());
+//        if (tagInput4.isChecked()) tags.add(tagInput4.getText().toString());
+//        if (tagInput6.isChecked()) tags.add(tagInput6.getText().toString());
+//        if (tagInput8.isChecked()) tags.add(tagInput8.getText().toString());
+//        if (tagInput10.isChecked()) tags.add(tagInput10.getText().toString());
 
         // get uploader name user input
         String uploaderName = uploaderNameFilterInput.getText().toString();
@@ -169,18 +169,18 @@ public class SearchActivity extends Activity {
                                 spinner.setVisibility(View.GONE);
 
                                 // parse data
-                                String[] imageUris = new String[data.length()];
+                                ArrayList<String> imageUris = new ArrayList<String>(data.length());
                                 for (int i = 0; i < data.length(); ++i) {
                                     try {
                                         JSONObject imageData = data.getJSONObject(i);
-                                        imageUris[i] = imageData.getString("URL");
+                                        imageUris.add(imageData.getString("URL"));
                                     } catch (JSONException e) {
                                         throw new RuntimeException(e.toString());
                                     }
                                 }
 
                                 // start ImageGridViewActivity
-                                if (imageUris.length > 0) {
+                                if (imageUris.size() > 0) {
                                     Intent i = new Intent(SearchActivity.this, ImageGridViewActivity.class);
                                     i.putExtra(ImageGridViewActivity.KEY_IMAGE_URIS, imageUris);
                                     startActivity(i);
@@ -227,16 +227,16 @@ public class SearchActivity extends Activity {
     /** clears the fields on the form */
     public void clearFields(View view) {
         uploaderNameFilterInput.setText("");
-        tagInput1.setChecked(false);
-        tagInput3.setChecked(false);
-        tagInput5.setChecked(false);
-        tagInput7.setChecked(false);
-        tagInput9.setChecked(false);
-        tagInput2.setChecked(false);
-        tagInput4.setChecked(false);
-        tagInput6.setChecked(false);
-        tagInput8.setChecked(false);
-        tagInput10.setChecked(false);
+//        tagInput1.setChecked(false);
+//        tagInput3.setChecked(false);
+//        tagInput5.setChecked(false);
+//        tagInput7.setChecked(false);
+//        tagInput9.setChecked(false);
+//        tagInput2.setChecked(false);
+//        tagInput4.setChecked(false);
+//        tagInput6.setChecked(false);
+//        tagInput8.setChecked(false);
+//        tagInput10.setChecked(false);
     }
 
     /////////////////////
@@ -246,15 +246,15 @@ public class SearchActivity extends Activity {
         feedbackText = (TextView) findViewById(R.id.feedback);
         spinner = (ProgressBar) findViewById(R.id.spinner);
         uploaderNameFilterInput = (EditText) findViewById(R.id.uploader_search_edit_text);
-        tagInput1 = (CheckBox) findViewById(R.id.checkBox1);
-        tagInput3 = (CheckBox) findViewById(R.id.checkBox3);
-        tagInput5 = (CheckBox) findViewById(R.id.checkBox5);
-        tagInput7 = (CheckBox) findViewById(R.id.checkBox7);
-        tagInput9 = (CheckBox) findViewById(R.id.checkBox9);
-        tagInput2 = (CheckBox) findViewById(R.id.checkBox2);
-        tagInput4 = (CheckBox) findViewById(R.id.checkBox4);
-        tagInput6 = (CheckBox) findViewById(R.id.checkBox6);
-        tagInput8 = (CheckBox) findViewById(R.id.checkBox8);
-        tagInput10 = (CheckBox) findViewById(R.id.checkBox10);
+//        tagInput1 = (CheckBox) findViewById(R.id.checkBox1);
+//        tagInput3 = (CheckBox) findViewById(R.id.checkBox3);
+//        tagInput5 = (CheckBox) findViewById(R.id.checkBox5);
+//        tagInput7 = (CheckBox) findViewById(R.id.checkBox7);
+//        tagInput9 = (CheckBox) findViewById(R.id.checkBox9);
+//        tagInput2 = (CheckBox) findViewById(R.id.checkBox2);
+//        tagInput4 = (CheckBox) findViewById(R.id.checkBox4);
+//        tagInput6 = (CheckBox) findViewById(R.id.checkBox6);
+//        tagInput8 = (CheckBox) findViewById(R.id.checkBox8);
+//        tagInput10 = (CheckBox) findViewById(R.id.checkBox10);
     }
 }
